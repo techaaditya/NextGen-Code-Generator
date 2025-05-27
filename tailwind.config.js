6e29f30302440bc6aab22ba5c7faee0a8bcb63a0
TechAaditya <sayhi.aaditya@gmail.com>
2025-05-27 22:19:56 +0545

    just

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
